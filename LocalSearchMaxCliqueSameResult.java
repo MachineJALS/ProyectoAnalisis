3fd27c4c846b2e97dc8456d06829ad85a6413d39
Roosevelt <roperez@estudiantec.cr>
2024-04-08 22:54:00 -0600

    new update

--- LocalSearchMaxCliqueSameResult.java
+++ LocalSearchMaxCliqueSameResult.java
@@ -1,6 +1,5 @@
 import java.util.HashSet;
 import java.util.Set;
-import java.util.ArrayList;
 
 public class LocalSearchMaxCliqueSameResult {
 
@@ -65,14 +64,14 @@ public class LocalSearchMaxCliqueSameResult {
     // Ejemplo de uso
     public static void main(String[] args) {
         // Crear un grafo de ejemplo
-        Graph graph = new Graph(7);
+        Graph graph = new Graph(11);
         /*graph.addEdge(1, 2);
         graph.addEdge(2, 5);
         graph.addEdge(3, 4);
         graph.addEdge(6, 4);
         graph.addEdge(1, 5); Prueba Uno Superada*/
 
-        /*
+
         graph.addEdge(1, 3);
         graph.addEdge(1, 5);
         graph.addEdge(2, 8);
@@ -91,23 +90,6 @@ public class LocalSearchMaxCliqueSameResult {
         graph.addEdge(7, 6);
         graph.addEdge(8, 9);
         graph.addEdge(9, 7);
-        */
-
-        // graph.addEdge(1,2);
-        // graph.addEdge(2,3);
-        // graph.addEdge(3,1);
-        // graph.addEdge(3,4);
-        // Otra prueba superada
-
-
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 3);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        
-
-
 
         // Encontrar el clique máximo
         Set<Integer> maxClique = localSearchMaxClique(graph);
