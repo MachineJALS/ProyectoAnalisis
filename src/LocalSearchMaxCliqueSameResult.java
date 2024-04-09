fa26d9f9333c0312fddf6b69ec2266f7c98cc9d7
Roosevelt <roperez@estudiantec.cr>
2024-04-09 16:56:01 -0600

    se cambio el nombre de algunos metodos de la clase Graph

--- src/LocalSearchMaxCliqueSameResult.java
+++ src/LocalSearchMaxCliqueSameResult.java
@@ -1,12 +1,11 @@
 import java.util.HashSet;
 import java.util.Set;
-import java.util.ArrayList;
 
 public class LocalSearchMaxCliqueSameResult {
 
     public static Set<Integer> localSearchMaxClique(Graph graph) {
         Set<Integer> candidates = new HashSet<>();
-        for (int v = 0; v < graph.getVertexCount(); v++) {
+        for (int v = 0; v < graph.getCantidadDeVertices(); v++) {
             candidates.add(v);
         }
 
@@ -20,7 +19,7 @@ public class LocalSearchMaxCliqueSameResult {
                 if (currentClique.contains(u)) continue; // Evita agregar vértices ya presentes en el clique
                 boolean isConnectedToAll = true;
                 for (int w : currentClique) {
-                    if (!graph.hasEdge(u, w)) {
+                    if (!graph.tieneConexion(u, w)) {
                         isConnectedToAll = false;
                         break;
                     }
@@ -40,23 +39,23 @@ public class LocalSearchMaxCliqueSameResult {
 
     // Clase Graph (simplificada)
     static class Graph {
-        private int vertexCount;
+        private int cantidadDeVertices;
         private boolean[][] adjacencyMatrix;
 
         public Graph(int vertexCount) {
-            this.vertexCount = vertexCount;
+            this.cantidadDeVertices = vertexCount;
             this.adjacencyMatrix = new boolean[vertexCount][vertexCount];
         }
 
-        public int getVertexCount() {
-            return vertexCount;
+        public int getCantidadDeVertices() {
+            return cantidadDeVertices;
         }
 
-        public boolean hasEdge(int u, int v) {
+        public boolean tieneConexion(int u, int v) {
             return adjacencyMatrix[u][v];
         }
 
-        public void addEdge(int u, int v) {
+        public void agregarConexion(int u, int v) {
             adjacencyMatrix[u][v] = true;
             adjacencyMatrix[v][u] = true;
         }
@@ -66,45 +65,45 @@ public class LocalSearchMaxCliqueSameResult {
     public static void main(String[] args) {
         // Crear un grafo de ejemplo
         Graph graph = new Graph(7);
-        /*graph.addEdge(1, 2);
-        graph.addEdge(2, 5);
-        graph.addEdge(3, 4);
-        graph.addEdge(6, 4);
-        graph.addEdge(1, 5); Prueba Uno Superada*/
+        // graph.agregarConexion(1, 2);
+        // graph.agregarConexion(2, 5);
+        // graph.agregarConexion(3, 4);
+        // graph.agregarConexion(6, 4);
+        // graph.agregarConexion(1, 5); //Prueba Uno //Superada*/
 
-        /*
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 5);
-        graph.addEdge(2, 8);
-        graph.addEdge(3, 2);
-        graph.addEdge(3, 5);
-        graph.addEdge(3, 4);
-        graph.addEdge(3, 6);
-        graph.addEdge(5, 4);
-        graph.addEdge(5, 6);
-        graph.addEdge(5, 9);
-        graph.addEdge(5, 7);
-        graph.addEdge(4, 6);
-        graph.addEdge(4, 2);
-        graph.addEdge(8, 7);
-        graph.addEdge(8, 6);
-        graph.addEdge(7, 6);
-        graph.addEdge(8, 9);
-        graph.addEdge(9, 7);
-        */
+        
+        // graph.agregarConexion(1, 3);
+        // graph.agregarConexion(1, 5);
+        // graph.agregarConexion(2, 8);
+        // graph.agregarConexion(3, 2);
+        // graph.agregarConexion(3, 5);
+        // graph.agregarConexion(3, 4);
+        // graph.agregarConexion(3, 6);
+        // graph.agregarConexion(5, 4);
+        // graph.agregarConexion(5, 6);
+        // graph.agregarConexion(5, 9);
+        // graph.agregarConexion(5, 7);
+        // graph.agregarConexion(4, 6);
+        // graph.agregarConexion(4, 2);
+        // graph.agregarConexion(8, 7);
+        // graph.agregarConexion(8, 6);
+        // graph.agregarConexion(7, 6);
+        // graph.agregarConexion(8, 9);
+        // graph.agregarConexion(9, 7);
+        
 
-        // graph.addEdge(1,2);
-        // graph.addEdge(2,3);
-        // graph.addEdge(3,1);
-        // graph.addEdge(3,4);
+        //  graph.agregarConexion(1,2);
+        //  graph.agregarConexion(2,3);
+        //  graph.agregarConexion(3,1);
+        //  graph.agregarConexion(3,4);
         // Otra prueba superada
 
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 3);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
+        graph.agregarConexion(0, 1);
+        graph.agregarConexion(0, 2);
+        graph.agregarConexion(0, 3);
+        graph.agregarConexion(1, 2);
+        graph.agregarConexion(1, 3);
         
 
 
