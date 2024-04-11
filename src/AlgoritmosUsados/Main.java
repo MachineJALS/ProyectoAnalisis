a1957db5978662265af7b5760a8685a0a147e998
Roosevelt <roperez@estudiantec.cr>
2024-04-11 16:59:51 -0600

    lorenzoPlayo

--- src/AlgoritmosUsados/Main.java
+++ src/AlgoritmosUsados/Main.java
@@ -303,7 +303,8 @@ public class Main {
         grafoDiez.agregarConexion(73, 78);
         grafoDiez.agregarConexion(74, 79);
         maxClique = new LocalSearchMaxCliqueSameResult().localSearchMaxClique(grafoDiez);
-        System.out.println("Clique Máximo del novena ejercicio método Busqueda Local: " + maxClique);
+        System.out.println("Clique Máximo del decimo ejercicio método Busqueda Local: " + maxClique);
+        // El clique esperado es [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
 
     //--------------------------------------------------------------------------------------------------------------
         // Apartado de BronKerboschMaxClique
